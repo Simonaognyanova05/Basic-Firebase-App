998cea55781675047dee672125de4d37a1cf5d8e
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-04-08 16:28:12 +0300

    Add getting playlist's title

--- src/user/yourPlaylists.js
+++ src/user/yourPlaylists.js
@@ -1,3 +1,6 @@
-module.exports = (req, res) => {
-    res.render('user/yourPlaylist', {title: 'Вашите плейлисти', user: req.session.user})
+const { getPlaylists } = require('../../requests/user/getPlaylist');
+
+module.exports = async (req, res) => {
+    const playlists = await getPlaylists(req, res);
+    res.render('user/yourPlaylist', {title: 'Вашите плейлисти', user: req.session.user, playlists})
 }
