177907fbd7679f7d20dd5677d64ba83d7c21635e
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-10-10 14:47:39 +0300

    Add register request to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const { registerAdmin } = require('./services/registerAdmin');
+
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
+app.post('/register', async(req, res) => {
+    await registerAdmin(req, res);
+})
 app.listen(2000);
---
+++ server/src/services/registerAdmin.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Admin = require('../models/Admin');
+
+const dbUrl = 'mongodb+srv://portfolio_db:TyvixWjDwKCgyF7d@portfolio.rv4xs.mongodb.net/blog_db';
+
+const connectionParams = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+async function registerAdmin(req, res) {
+    await mongoose.connect(dbUrl, connectionParams);
+
+    const { username, password } = req.body;
+
+    try {
+        const hashedPass = await bcrypt.hash(password, 10);
+
+        const admin = new Admin({
+            username,
+            hashedPass
+        });
+
+        await admin.save();
+        return res.status(200).json({ _id: admin._id, username: admin.username });
+    } catch (e) {
+        throw e;
+    }
+}
+
+module.exports = { registerAdmin };
