26e6fc77aa3f37cb942bee2ecfc2d4a1869fda52
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-10-10 15:28:32 +0300

    Add get request

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { registerAdmin } = require('./services/registerAdmin');
 const { loginAdmin } = require('./services/loginAdmin');
 const { createTopic } = require('./services/createTopic');
+const { getTopics } = require('./services/getTopics');
 
 
 
@@ -11,15 +12,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', async(req, res) => {
+app.post('/register', async (req, res) => {
     await registerAdmin(req, res);
 });
 
-app.post('/login', async(req, res) => {
+app.post('/login', async (req, res) => {
     await loginAdmin(req, res);
 });
 
-app.post('/createTopic', async(req, res) => {
+app.post('/createTopic', async (req, res) => {
     await createTopic(req, res);
 });
+
+app.get('/topics', async (req, res) => {
+    let topics = await getTopics(req, res);
+    return res.json(topics);
+})
 app.listen(2000);
