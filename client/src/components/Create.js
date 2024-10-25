c3ffc3a955cbffe0d32197d8ee3b02a7ad4d6a50
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-10-25 14:37:05 +0300

    Add create request

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,8 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import { create } from '../services/create';
+
 export default function Create() {
+    const navigate = useNavigate();
+
+    const createHandler = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const { title, description, img } = Object.fromEntries(formData);
+
+        let data = await create(title, description, img);
+
+        if (data.status === 200) {
+            alert('Статията е създаденa успешно!');
+            navigate('/');
+        }
+
+    }
     return (
         <>
             <h1>Създаване на тема</h1>
-            <form>
+            <form onSubmit={createHandler}>
                 <label htmlFor="title">Заглавие</label>
                 <input type="text" name="title" />
                 <label htmlFor="description">Описание</label>
@@ -10,6 +29,7 @@ export default function Create() {
 
                 <label htmlFor="img">Изображение</label>
                 <input type="text" name="img" />
+
                 <input type="submit" name="Създаване" />
             </form>
         </>
