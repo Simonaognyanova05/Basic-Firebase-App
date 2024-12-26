fa22875597cded020eea595159422b47d69be57f
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-12-26 21:42:04 +0200

    Prepare project for work

---
+++ firebase-app/src/config/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDwtt2n0xHsGI-E6jFW95UGHXtM_4M0zhM",
+  authDomain: "basic-app-1e303.firebaseapp.com",
+  projectId: "basic-app-1e303",
+  storageBucket: "basic-app-1e303.firebasestorage.app",
+  messagingSenderId: "251150378234",
+  appId: "1:251150378234:web:a944ca8d6002cfb349d543"
+};
+
+const app = initializeApp(firebaseConfig);
