f3d15b6f342a6dc3d45e70781dd5a7d9db331a09
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-12-26 21:52:05 +0200

    Add file for firebase configuration

--- firebase-app/src/config/firebase.js
+++ firebase-app/src/config/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDwtt2n0xHsGI-E6jFW95UGHXtM_4M0zhM",
   authDomain: "basic-app-1e303.firebaseapp.com",
@@ -9,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
