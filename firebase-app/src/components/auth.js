9b511a788c59288f07e0ee14765d0b6f2074205f
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-12-27 16:30:52 +0200

    Add register user

--- firebase-app/src/components/auth.js
+++ firebase-app/src/components/auth.js
@@ -1,13 +1,20 @@
 import { auth } from '../config/firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
 
 export const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = async (e) => {
+        await createUserWithEmailAndPassword(auth, email, password);
+    }
     return (
         <div>
-            <input placeholder="Email..." />
-            <input placeholder="Password..." />
+            <input placeholder="Email..." onChange={(e) => setEmail(e.target.value)} />
+            <input placeholder="Password..." type='password' onChange={(e) => setPassword(e.target.value)} />
 
-            <button>Sign In</button>
+            <button onClick={signIn}>Sign In</button>
         </div>
     );
 }
