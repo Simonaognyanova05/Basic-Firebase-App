75a4d5aaf4f49bf5232540de8c44e980eec435f9
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-01-02 21:49:24 +0200

    Add create movie request

--- firebase-app/src/App.js
+++ firebase-app/src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 import { Auth } from './components/auth.js';
 import { db } from './config/firebase.js';
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 
 function App() {
   const [movieList, setMovieList] = useState([]);
+
+  //Movie Info
+  const [newMovieTitle, setNewMovieTitle] = useState("");
+  const [newReleaseDate, setNewReleaseDate] = useState(0);
+  const [isNewMovieOskar, setIsNewMovieOskar] = useState(false);
+
 
   const moviesCollectionRef = collection(db, "movies");
 
@@ -22,12 +28,29 @@ function App() {
     }
 
     getMovieList();
-  }, [])
+  }, []);
+
+  const onSumbitMovie = async () => {
+    try {
+      await addDoc(moviesCollectionRef, { title: newMovieTitle, releaseDate: newReleaseDate, receivedAnOskar: isNewMovieOskar });
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
     <h1>
       <Auth />
 
+      <div>
+        <input placeholder='Movie title' onChange={(e) => setNewMovieTitle(e.target.value)} />
+        <input placeholder='Release date' type='number' onChange={(e) => setNewReleaseDate(Number(e.target.value))} />
+        <input type='checkbox'
+          checked={isNewMovieOskar}
+          onChange={(e) => setIsNewMovieOskar(e.target.checked)} />
+        <label>Reseived Oskar</label>
+        <button onClick={onSumbitMovie}>Submit Movie</button>
+      </div>
       <div>
         {movieList.map(movie => (
           <div>
